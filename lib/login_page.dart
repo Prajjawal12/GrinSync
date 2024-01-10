898e4b03950a076d9b68a27cc2b43430f475f1ed
Prajjawal <prajjawaldy.zhcet@gmail.com>
2024-01-10 23:13:24 +0530

    improved the home page UI

---
+++ lib/login_page.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(186, 31, 60, 87),
+      body: Column(
+        children: [
+          const Text(
+            'Hello, Welcome Back!',
+            style: TextStyle(
+                color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          const Text(
+            'Login To Continue',
+            style: TextStyle(color: Colors.white),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Username',
+              border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.5),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.5),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {
+                print('clicked');
+              },
+              style: TextButton.styleFrom(foregroundColor: Colors.white),
+              child: const Text('Forgot Password?'),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+              onPressed: () {
+                print("Login clicked");
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              child: const Text(
+                'Log In',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Or sign in with',
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
+                ),
+              ),
+            ),
+            onPressed: () {
+              print('Google Is Clicked');
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/google.png',
+                  width: 22,
+                  height: 22,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                const Text('Login With Google')
+              ],
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
+                ),
+              ),
+            ),
+            onPressed: () {},
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/facebook.png',
+                  width: 22,
+                  height: 22,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                const Text('Login with Facebook')
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                'Don\'t have an account? ',
+                style: TextStyle(color: Colors.white),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationColor: Colors.amber),
+                ),
+                style: TextButton.styleFrom(foregroundColor: Colors.amber),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,76 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue.shade900,
-        body: Column(
-          children: [
-            const Text('Hello, Welcome Back!'),
-            const Text('Login To Continue'),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Username'),
-            ),
-            const TextField(
-              decoration: InputDecoration(hintText: 'Password'),
-            ),
-            TextButton(
-              onPressed: () {
-                print('clicked');
-              },
-              child: const Text('Forgot Password?'),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.transparent),
-              ),
-              onPressed: () {
-                print("Login clicked");
-              },
-              child: const Text('Log In'),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text('Or sign in with'),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all(Colors.transparent)),
-              onPressed: () {
-                print('Google Is Clicked');
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/google.png',
-                    width: 22,
-                    height: 22,
-                  ),
-                  Image.asset(
-                    'assets/images/facebook.png',
-                    width: 22,
-                    height: 22,
-                  )
-                ],
-              ),
-            ),
-            Row(
-              children: [
-                Text('Don\'t have an account? '),
-                TextButton(onPressed: () {}, child: Text('Sign Up'))
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
